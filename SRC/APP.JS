e3f6b832a11515e42f80a7b8621ac42ae0900445
denislincolnbu <denislcdias@gmail.com>
2026-02-27 20:39:33 -0300

    Enviando estrutura final com pastas src, screens, services e store

---
+++ SRC/APP.JS
@@ -0,0 +1,11 @@
+import { Provider } from 'react-redux';
+import Routes from './src/routes';
+import store from './src/store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
+}
